96ac619f1b6154329e7997851adcd51589705da8
hari-44456 <naraharipapshetwar@gmail.com>
2021-10-14 11:27:32 +0530

    added windows vm configuration

--- aws/main.tf
+++ aws/main.tf
@@ -21,16 +21,9 @@ resource aws_security_group "myapp-sg" {
     name = "${var.prefix}-sg"
 
     ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
@@ -66,9 +59,19 @@ resource "aws_key_pair" "deployer" {
   public_key = file(var.public_key_location)
 }
 
+variable admin_username {
+  default="narahari"
+}
+
+variable admin_password {
+  default="N@rahari12345!"
+}
+
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+
+  ami           = "ami-0428fc1ee1bde045a"
   instance_type = "t2.micro"
+
   key_name = aws_key_pair.deployer.key_name
 
   subnet_id = module.vpc.public_subnets[0]
@@ -76,23 +79,44 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.myapp-sg.id]
   associate_public_ip_address = true
 
-  provisioner "remote-exec" {
-  
-    inline = [
-     "mkdir newDir",
-     "rmdir newDir"
-    ]
-    
-    connection {
-      type = "ssh"
-      host = self.public_ip
-      user = "ec2-user"
-      private_key = file(var.private_key_location)
-    }
+   user_data     = <<EOF
+    <powershell>
+    net user ${var.admin_username} '${var.admin_password}' /add /y
+    net localgroup administrators ${var.admin_username} /add
+    winrm quickconfig -q
+    winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="300"}'
+    winrm set winrm/config '@{MaxTimeoutms="1800000"}'
+    winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+    winrm set winrm/config/service/auth '@{Basic="true"}'
+    netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localport=5985 action=allow
+    netsh advfirewall firewall add rule name="WinRM 5986" protocol=TCP dir=in localport=5986 action=allow
+    net stop winrm
+    sc.exe config winrm start=auto
+    net start winrm
+    </powershell>
+    EOF
+
+  connection {
+    host = coalesce(self.public_ip, self.private_ip)
+    type = "winrm"
+    timeout = "10m"
+    user = var.admin_username
+    password = var.admin_password
   }
 
+  provisioner "file" {
+    source = "abc.txt"
+    destination = "C:/test.txt"
+  }
+
+  provisioner "remote-exec" {
+      inline = [
+        "mkdir helloworld",
+      ]
+    }
+
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user   -i ${self.public_ip}, --private-key ${var.private_key_location} playbook.yml"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.admin_username}   -i ${self.public_ip}, --private-key ${var.private_key_location} playbook.yml"
   }
 
   tags = {
