2df1f10cab1cf9f91da0eb34b6f2240f75f24bb4
kakarot_6909 <rahul.naravadkar@gmail.com>
2021-10-14 19:01:39 +0530

    Add. Adds ssh security rule

--- azure-win/main.tf
+++ azure-win/main.tf
@@ -66,6 +66,17 @@ resource "azurerm_network_security_group" "example" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
   tags = {
     environment = "DEV"
