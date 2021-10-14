6683a9571144599b9fad20bf0113e6a1256da091
kakarot_6909 <rahul.naravadkar@gmail.com>
2021-10-14 15:31:53 +0530

    Add. Adds the azure windows config

---
+++ azure-win/main.tf
@@ -0,0 +1,180 @@
+variable admin_username {
+  default="rahul"
+}
+variable admin_password {
+  default="Rahul@2410"
+}
+variable "os_ms" {
+  description = "Operating System for Database (MSSQL) on the Production Environment"
+  type        = map(string)
+
+  default = {
+    publisher   =   "MicrosoftWindowsServer"
+    offer       =   "WindowsServer"
+    sku         =   "2019-Datacenter"
+    version     =   "latest"
+  }
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "WinRM"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5985"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "DEV"
+  }
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-pip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                            = "${var.prefix}-vm"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  # size                            = "Standard_F2"
+  # admin_username                  = var.user
+  vm_size                         = "Standard_DS1_v2"
+
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  storage_image_reference {
+    publisher = "${var.os_ms["publisher"]}"
+    offer     = "${var.os_ms["offer"]}"
+    sku       = "${var.os_ms["sku"]}"
+    version   = "${var.os_ms["version"]}"
+  }
+  storage_os_disk {
+    name              = "RahulOS"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  
+  os_profile {
+    computer_name  = "RahulOS"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
+    custom_data    = "${file("./files/winrm.ps1")}"
+  }
+  os_profile_windows_config {
+    provision_vm_agent = "true"
+    timezone           = "Romance Standard Time"
+    winrm {
+      protocol = "http"
+    }
+    # Auto-Login's required to configure WinRM
+    additional_unattend_config {
+      pass         = "oobeSystem"
+      component    = "Microsoft-Windows-Shell-Setup"
+      setting_name = "AutoLogon"
+      content      = "<AutoLogon><Password><Value>${var.admin_password}</Value></Password><Enabled>true</Enabled><LogonCount>1</LogonCount><Username>${var.admin_username}</Username></AutoLogon>"
+    }
+    additional_unattend_config {
+      pass         = "oobeSystem"
+      component    = "Microsoft-Windows-Shell-Setup"
+      setting_name = "FirstLogonCommands"
+      content      = "${file("./files/FirstLogonCommands.xml")}"
+    }
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host     = "${azurerm_public_ip.main.ip_address}"
+      type     = "winrm"
+      port     = 5985
+      https    = false
+      timeout  = "5m"
+      user     = "${var.admin_username}"
+      password = "${var.admin_password}"
+    }
+    inline = [
+      "mkdir helloworld",
+    ]
+  }
+
+  # provisioner "local-exec" {
+  #   command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.user} -i ${self.public_ip_address}, --private-key ${var.private_key_location} playbook.yml"
+  # }
+}
