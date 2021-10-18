b4c18634c33d55b26d9084fd500c78502510b5ad
pnirmale <nirmaleprathamesh@gmail.com>
2021-10-18 22:49:14 +0530

    AMI and playbook yml

--- aws-win/providers.tf
+++ aws-win/providers.tf
@@ -1,6 +1,4 @@
 provider "aws" {
      region ="ap-south-1"
     // if not specified here then we have to set it via ennvironment variable or run aws configure
-    access_key ="AKIAZOLLOARNN536YIHJ"
-   secret_key ="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"
 }
