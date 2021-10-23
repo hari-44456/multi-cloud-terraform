8694ad130f9a5609c2e9f9c8afbbab4500ee9a4a
hari-44456 <naraharipapshetwar@gmail.com>
2021-10-23 12:23:31 +0530

    Removed OpenSSH Powershell Commands From user_data

--- aws-win/main.tf
+++ aws-win/main.tf
@@ -71,10 +71,6 @@ resource "aws_instance" "web" {
     net stop winrm
     sc.exe config winrm start=auto
     net start winrm
-    Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
-    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-    Start-Service sshd
-    Set-Service -Name sshd -StartupType 'Automatic'
     
     Write-Host "Config custom"
     $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
